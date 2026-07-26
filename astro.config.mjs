a04f82806b7f03dc73fb78c657097cbd4b54516b
vlad <vlad1slav200508@yandex.ru>
2026-07-27 02:32:13 +0300

    fix: set default server output for correct theme and language rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
+  output: "server",
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: "standalone",
+  }),
 });
